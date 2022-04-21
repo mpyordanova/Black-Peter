f05b42f639c0ecf612b0d63335b22499f7fe2f5a
Paramjit Kaur <paramjitkaur@Mimosas-MacBook-Pro.local>
2022-04-20 20:55:32 -0400

    commiting changes to code

--- BlackPeter/BlackPeter.js
+++ BlackPeter/BlackPeter.js
@@ -5,13 +5,9 @@ const drawBtn2 = document.getElementById("Draw card-Petya")
 const MimiPlay = document.getElementById("Mimi-hand")
 const PetyaPlay = document.getElementById("Petya-hand")
 
-document.addEventListener("click",game);
- drawBtn.addEventListener("click",function(event){
-     const result = draw(cardsMimi, cardsPetya)
- })
- drawBtn2.addEventListener("click",function(event){
-  const result = draw(cardsPetya, cardsMimi)
-})
+// Removed it because it runs the game function twice loads the arrays twice.
+// document.addEventListener("click",game);
+ 
 
 
 // Create 3 arrays - one with all the cards and 2 for each player
@@ -20,7 +16,7 @@ let cardsMimi = [];
 let cardsPetya = [];
 
 function game(){
-    let cardsColors = ['red', 'red', 'orange', 'blue', 'green', 'purpule', 'yellow', 'white', 'pink', 'black','red', 'orange', 'blue', 'green', 'purpule', 'yellow', 'white', 'pink'];
+    let cardsColors = ['red', 'red', 'orange', 'orange', 'blue', 'green', 'purpule', 'yellow', 'white', 'pink', 'black','red', 'orange', 'blue', 'green', 'purpule', 'yellow', 'white', 'pink'];
 for(let i=0; i < cardsColors.length; i++)
 {
     if(i % 2 === 0)
@@ -37,43 +33,67 @@ console.log(cardsMimi);
 console.log(cardsPetya);
 let MimiUniqueArray = removeDuplicates(cardsMimi);
 let PetyaUniqueArray = removeDuplicates(cardsPetya);
- console.log(MimiUniqueArray)
+ console.log(MimiUniqueArray);
+ console.log(PetyaUniqueArray);
 }
-// function draw card
-function draw(self, opponent){
-    let ranCard = Math.floor(Math.random() * opponent.length);
-  
-// 1. get your random card index (done)
-// 2. use the javascript splice array method to grab the correct card and push it into the other array.
-// 3. run removeduplicates on the player's array.
-// 4. create a new object with both self and opponent arrays in it
-// // 5. when you call the draw function set the arrays that were used to the new values
-
-
-
-
+// removed cardsPetya from the end to test why it is giving me 2 rounds of cards!
+drawBtn.addEventListener("click",function(event){const result = draw(cardsMimi, cardsPetya)})
+drawBtn2.addEventListener("click",function(event){const result = draw(cardsPetya, cardsMimi)})
+// create new array without the duplicate cards(they must go in a separate deck)
+function removeDuplicates(arr) 
+{
+        let counts = arr.reduce(function(prevItem, currItem)
+    {
+        prevItem[currItem] = (prevItem[currItem] || 0) + 1;
+        return prevItem;
+    }, {});
+         return Object.keys(counts).reduce(function(arr, item) 
+        {
+                if (counts[item] === 1) 
+            {
+                arr.result.push(item);
+            }
+                else 
+                {
+                    arr.duplicates.push(item)
+                }
+                    return arr;
+        }, 
+                    {
+                        duplicates:[],result:[]
+                    });
 }
-document.getElementById("Draw-card-Mimi").onclick = draw
+
+// function Mimi draws card from Petya's array
+function drawMimi()
+{
+    let ranCard = Math.floor(Math.random() * cardsPetya.length);
+    let card = cardsPetya.splice(ranCard,1)
+    cardsMimi.push(card)
+    const result = removeDuplicates(cardsMimi)
+    cardsMimi = result.result
+    return card;
+}
+    console.log(draw(cardsPetya))
+
+    document.getElementById("Draw-card-Mimi").onclick = draw
 // draw not draw(), because if I have () it will envokes the function.
 
-// create new array without the duplicate cards. It does not fill the arrays with cards after it removes the duplicates!?We
+// function Petya draws card from Mimi's array
+function drawPetya()
+{
+    let ranCard = Math.floor(Math.random() * cardsMimi.length);
+    let card = cardsMimi.splice(ranCard,1)
+    cardsPetya.push(card)
+    const result = removeDuplicates(cardsPetya)
+    cardsPetya = result.result
+    return card;
+}  
+    console.log(draw(cardsMimi))
 
-   function removeDuplicates(arr) {
-  let counts = arr.reduce(function(prevItem, currItem) {
-    prevItem[currItem] = (prevItem[currItem] || 0) + 1;
-    return prevItem;
-  }, {});
-  return Object.keys(counts).reduce(function(arr, item) {
-    if (counts[item] === 1) {
-      arr.result.push(item);
-    }
-    else{arr.duplicates.push(item)
-    }
-    return arr;
-  }, {
-    duplicates:[],result:[]
-  });
-}
+    document.getElementById("Draw-card-Petya").onclick = draw
+
+
 
   
  
